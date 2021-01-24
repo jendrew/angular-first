712c151eb89fe189ef9444b6835861cde0976768
Jędrzej Kołtunowicz <jedrzej.koltunowicz@opi.org.pl>
2021-01-24 11:56:19 +0100

    First commit

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+
+@Component({
+    selector: 'event-thumbnail',
+    templateUrl: './event-thumbnail.component.html',
+    styleUrls: ['./event-thumbnail.component.css']
+})
+
+export class EventThumbnailComponent {
+    @Input() event: any
+    @Output() eventClick = new EventEmitter()
+    someProperty:any = "some value"
+    starred: boolean = false
+
+    handleClickMe() {
+        console.log('ThumbnailComponent: Click!')
+        this.eventClick.emit(this.event.name)
+    }
+
+    logFoo() {
+        console.log('foo')
+    }
+
+    giveStar() {
+        if (this.starred == false) {
+            this.starred = true
+        } else {
+            this.starred = false
+        }
+    }
+}
